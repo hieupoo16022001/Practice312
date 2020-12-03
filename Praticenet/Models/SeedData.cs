9707ec3b074ee93a4ac5e09e1ef2162d33831b95
hieupoo16022001 <54589137+hieupoo16022001@users.noreply.github.com>
2020-12-03 15:53:06 +0700

    sss

---
+++ Praticenet/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Praticenet.Models
+{
+    public static class SeedData
+    {
+
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Nguyen Trung Hieu",
+                        Luong = 1000 ,
+                        Category = "Boss"
+                    },
+                  
+                    new Product
+                    {
+                        Name = "Trieu Cao Chinh",
+                        Luong = 100,
+                        Category = "Security"
+                    },
+                     new Product
+                     {
+                         Name = "Nguyen Van Hoang",
+                         Luong = 100,
+                         Category = "Security"
+                     },
+                      new Product
+                      {
+                          Name = "Vu Dinh Hieu",
+                          Luong = 100,
+                          Category = "Security"
+                      }
+                     
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
